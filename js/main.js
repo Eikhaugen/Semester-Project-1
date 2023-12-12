f474d7ca77342aea5cdf96796c644e9866111581
Thomas Eikhaugen <thomas.eikhaugen@gmail.com>
2023-12-12 17:13:57 +0100

    added display flex to form

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 let menu = document.getElementById("menu");
 let menuIcon = document.getElementById("menuIcon");
 let closeIcon = document.getElementById("closeIcon");
-let body = document.body; // Select the body element
+let body = document.body;
 
 document.getElementById("toggleBtn").addEventListener("click", function () {
     if (menu.style.display === "none" || menu.style.display === "") {
         menu.style.display = "block";
         menuIcon.style.display = "none";
         closeIcon.style.display = "block";
-        body.classList.add("menu-open"); // Add a class to the body when the menu is open
+        body.classList.add("menu-open");
     } else {
         menu.style.display = "none";
         menuIcon.style.display = "block";
         closeIcon.style.display = "none";
-        body.classList.remove("menu-open"); // Remove the class when the menu is closed
+        body.classList.remove("menu-open");
     }
 });
 
